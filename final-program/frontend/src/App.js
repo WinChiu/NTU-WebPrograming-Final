948aea83310d3fc28a8ea856d7ce403f81411dcd
Win Chiu <win881206@gmail.com>
2021-06-01 12:16:55 +0800

    setup

---
+++ final-program/frontend/src/App.js
@@ -0,0 +1,41 @@
+import "./style/index.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Account from "./components/Account";
+import Header from "./components/note/Header";
+import Note from "./components/note/Note_index";
+import AddNote from "./components/note/AddNote";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [memberName, setMemberName] = useState("default name");
+
+  return (
+    <Router>
+      <Navbar memberName={memberName} isLogin={isLogin} />
+      <Switch>
+        <Route exact path="/">
+          <About />
+        </Route>
+        <Route path="/note">
+          <Header />
+          <Note />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login memberName={memberName} setIsLogin={setIsLogin} />
+        </Route>
+        <Route path="/account">
+          <Account />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
