d3723d70b73c1aafe17c7cdf15bdafd87178cccb
Win Chiu <win881206@gmail.com>
2021-06-30 16:07:19 +0800

    test31

--- final-program/backend/server.js
+++ final-program/backend/server.js
@@ -67,7 +67,7 @@ mongoose.set("useFindAndModify", false);
 // });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("../build"));
 
   // // Hande SPA
   // app.use(/.*/, (req, res) => {
